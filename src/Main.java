ae98eb1ebe812db62ac81ce7c4e6a376d39cfa1f
Mikalai Bezmen <Mikalai_Bezmen@epam.com>
2014-08-15 15:52:59 +0300

    Update Main.java
    Add

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("ёёёёё!");
+        System.out.println("ёёёёёёёё!");
         System.out.println("ёёё!");
     }
 }
